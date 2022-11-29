94de7e05e5f97ccde963f1d46781d456e18851a2
Spencer Cheng <89827794+spencercheng@users.noreply.github.com>
2022-11-29 16:50:08 -0700

    design front-end

--- healthtracker/settings.py
+++ healthtracker/settings.py
@@ -79,9 +79,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'healthtracker',
         'USER' : 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': '0067Tc0287!',
         'HOST': 'localhost',
-        'PORT' : '5433'
     }
 }
 
