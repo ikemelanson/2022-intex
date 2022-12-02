d8a2ff1d996251950f234d403732d09a99626ac5
ikemelanson <ikemelanson@gmail.com>
2022-12-01 21:42:45 -0700

    for spencer

--- journals/urls.py
+++ journals/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path("dashboard/", dashboardPageView, name="dashboard"),
     path("newFood/", newFoodPageView, name="newFood"),
     path("commitFood/", commitFood, name="commitFood"),
+    path("viewSerums/", viewSerums, name="viewSerums"),
+    path("editSerum/", editSerum, name="editSerum"),
     path("searchFood/", searchFoodPageView, name="searchFood"),
     path("login/", auth_view.LoginView.as_view(template_name='healthtracker/login.html'), name="login"),
     path("accountregister/", accountRegisterView, name="account"),
